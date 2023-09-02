9acbef92adc6c495bdb5b1ce6090cf821b26dace
writetoyouraj <writetoyouraj@gmail.com>
2023-09-02 14:34:54 -0500

    First commit

---
+++ src/server/Server.java
@@ -0,0 +1,33 @@
+package server;
+
+import exception_handler.ChatAppException;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.*;
+
+
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
+
+    public Server() {
+
+        try{
+            serverSocket = new ServerSocket(7878);
+            logger.severe("Server has started...");
+            logger.info("Waiting for connection ...");
+
+        }catch (IOException e) {
+            throw new ChatAppException();
+        }
+    }
+
+
+
+}
