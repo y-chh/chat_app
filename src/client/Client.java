f1c7fd0bdadb9f91481e9d302a15c7314f127258
y-chh <142819062+y-chh@users.noreply.github.com>
2023-09-11 11:24:35 -0500

    Publishing from dev branch

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,49 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
+
+        final String SERVER_HOST = "localhost";
+        final int SERVER_PORT = 7878;
+
+        try{
+            Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Connected to the server");
+            Thread receiveThread = new Thread(()-> {
+                try{
+                    while(true) {
+                        String serverMessage = reader.readLine();
+                        if(serverMessage == null) {
+                            System.out.println("Server Disconnected");
+                            break;
+                        }
+                        System.out.println(serverMessage);
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            receiveThread.start();
+            while(true) {
+                String clientMessage = scanner.nextLine();
+                if(clientMessage.equalsIgnoreCase("quit")) {
+                    break;
+                }
+                writer.println(clientMessage);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
 
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.*;
+import java.io.*;
 
 
 
@@ -13,6 +14,12 @@ public class Server {
 
     private ServerSocket serverSocket;
     private Socket socket;
+
+    private BufferedReader readClient, readServer;
+
+    private PrintWriter write;
+
+    private Runnable t1, t2; // implement multi threading
 
     private static final Logger logger = Logger.getLogger(Server.class.getName());
 
@@ -22,13 +29,80 @@ public class Server {
             serverSocket = new ServerSocket(7878);
             logger.severe("Server has started...");
             logger.info("Waiting for connection ...");
+            socket = serverSocket.accept(); // when server is accepting the request, data passed through will be read by BufferedReader obj
+            InputStream inputStream = socket.getInputStream(); // hold the incoming stream of data into this object.
+            OutputStream outputStream = socket.getOutputStream();
+            // read data from the input stream
+            readClient = new BufferedReader(new InputStreamReader(inputStream));
+            write = new PrintWriter(outputStream);
 
+            // create functions to perform reading and writing
+            readingData();
+            writingData();
 
         }catch (IOException e) {
             throw new ChatAppException();
         }
     }
 
+    public void readingData() {
+        t1 = () -> {
+            logger.info("Reader Started...");
+            while(true) {
+                try {
+                    String clientMessage = readClient.readLine();
+                    if(clientMessage.equalsIgnoreCase("quit")){
+                        logger.info("Client has initiated chat termination.");
+                        break;
+                    }
+                    logger.info("Client : " +clientMessage);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        new Thread(t1).start();
+    }
+
+    public void writingData() {
+        t2 = () -> {
+            while(true) {
+                try{
+                    readServer = new BufferedReader(new InputStreamReader(System.in)); // read data from console
+                    String serverResponse = readServer.readLine();
+                    write.println(serverResponse);
+                    write.flush();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(t2).start();
+    }
 
 
+    public static void main(String[] args) {
+        Server sr = new Server();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
